c92aa933236e33914c6178fa87e3eea1a409661e
root <root@DESKTOP-BLPH810>
2024-02-07 01:16:16 +0200

    Exepctions in python

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+ y = 0   
  i = 0   
  try :
     while i is not x :
      print("{}".format(my_list[i]),end='')
+     y+=1
      i+=1
  except IndexError:
   None
  print()
- return i
+ return y
