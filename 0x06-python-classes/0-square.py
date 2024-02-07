d89cb5cf3434b522fd7eae8504c40442b704dec2
root <root@DESKTOP-BLPH810>
2024-02-07 09:35:07 +0200

    classes and objects in python

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,29 +1,5 @@
 #!/usr/bin/python3
-
 class Square:
-    """
-    Class Square that defines a square.
-    """
-
-    def __init__(self):
-        self.side = 0
-        self.dict_ = {}
-
-    def __repr__(self):
-        return f'Square(side={self.side})'
-
-    def __str__(self):
-        return f'Square with side length {self.side}'
-
-    def update_side(self, side_length):
-        """
-        Update the side length of the square.
-        """
-        self.side = side_length
-        self.dict_['side'] = self.side
-
-    def area(self):
-        """
-        Calculate the area of the square.
-        """
-        return self.side ** 2
+    '''Class Square that defines a square.
+    '''
+    pass
