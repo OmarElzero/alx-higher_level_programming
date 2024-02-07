7abb2643064fdbf3ff7fb22735dec0b7806cca12
root <root@DESKTOP-BLPH810>
2024-02-07 04:51:41 +0200

    classes and objects in python

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -9,7 +9,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
 
     def area(self):
         """ Method that returns the square are of the object
