c8dbd0e22b9389dc67ba0059fffd78c1b3590815
root <root@DESKTOP-BLPH810>
2024-02-21 01:54:43 +0200

    file i/o task

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
-"""ddefines a student class"""
+"""Defines a Student Class."""
 
 
 class Student:
-    """"a class Student that defines a student"""
+    """Defines a student."""
+
     def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ returninh a dic repesintation of a  student instance"""
-        try:
-            for attr in attrs:
-                if type(attr) is not str:
-                    return self.__dict__
-        except Exception:
-            return self.__dict__
-        my_dict = dict()
-        for key, in value in self.__dict__.items():
-            if key in attrs:
-                my_dict[key] = value
+        """Get a dictionary representation of the Student.
 
-        return my_dict
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+
+        if isinstance(attrs, list) and all(isinstance(ele, str) for ele in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
