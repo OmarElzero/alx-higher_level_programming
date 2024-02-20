c491c9fd800ecb3305c094bca6eb27315e583dbb
root <root@DESKTOP-BLPH810>
2024-02-21 00:53:35 +0200

    file i/o task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,7 +6,7 @@ import sys
 save_to_json_fil = __import__('5-save_to_json_file.py').save_to_json_file
 load_from_json_fil = __import__('6-load_from_json_file.py').load_from_json_file
 
-arglist = list(sys.argv[1])
+arglist = list(sys.argv[1:])
 try:
     old_data = load_from_json_fil('add_item.json')
 except Exception:
