01c3ff904ca1f37af175b8cc8d0512ef7697d5d7
root <root@DESKTOP-BLPH810>
2024-02-20 21:08:01 +0200

    solving file i/o task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, 'r') as file:
+    with open(filename, encoding='utf-8') as file:
         content = file.read()
-        print(content)
+        print(content, end="")
