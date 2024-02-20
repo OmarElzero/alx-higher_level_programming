ad5b80d1a6f15c504a81524beac4743d99c257dc
root <root@DESKTOP-BLPH810>
2024-02-21 00:11:22 +0200

    file i/o task

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defining save_to_json_file function"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """save_to_json_file """
+    with open(filename,'w') as file:
+        file = json.loads(my_obj)
+        return file
