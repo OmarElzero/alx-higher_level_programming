a302f43b8d11d89641dac2239d1a0e22c6b45b34
root <root@DESKTOP-BLPH810>
2024-06-19 01:17:34 +0300

    adding file for java worm up

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+function addMeMaybe (number, theFunction) {
+  const incrementedNumber = number + 1;
+  theFunction(incrementedNumber);
+}
+module.exports = { addMeMaybe };
--- 0x12-javascript-warm_up/tempCodeRunnerFile.js
+++ 0x12-javascript-warm_up/tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-callMeMoby
+addMeMaybe
