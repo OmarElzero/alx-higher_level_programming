59bc200d10d3d1f23f39dc28205f84db3419b6ee
root <root@DESKTOP-BLPH810>
2024-06-19 00:07:04 +0300

    adding file for java worm up

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+if (process.argv.length <= 3) {
+  console.log(1);
+} else {
+  let max1 = -Infinity;
+  let max2 = -Infinity;
+
+  for (let i = 2; i < process.argv.length; i++) {
+    const num = parseInt(process.argv[i]);
+    if (num > max1) {
+      max2 = max1;
+      max1 = num;
+    } else if (num > max2 && num < max1) {
+      max2 = num;
+    }
+  }
+
+  console.log(max2);
+} 
