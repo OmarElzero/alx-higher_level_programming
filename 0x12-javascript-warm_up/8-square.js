bcc7bbc00baf5be452ac52561b519cc3b4176600
root <root@DESKTOP-BLPH810>
2024-06-18 23:06:38 +0300

    adding file for java worm up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,7 +6,7 @@ if (!parseInt(num)) {
   for (let i = 0; i < num; i++) {
     let row = '';
     for (let j = 0; j < num; j++) {
-      row+='X';
+      row += 'X';
     }
     console.log(row);
   }
