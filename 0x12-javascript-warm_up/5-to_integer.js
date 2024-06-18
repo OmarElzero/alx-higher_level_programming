748dcc41c4ecaa629b831b7dd443b44e770295f9
root <root@DESKTOP-BLPH810>
2024-06-18 22:28:17 +0300

    adding file for java worm up

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const argsValue = process.argv[2]; 
+
+if (Number.isInteger(parseInt(argsValue))) {
+  console.log(`My number: ${parseInt(argsValue)}`);
+} else {
+  console.log('Not a number');
+}
